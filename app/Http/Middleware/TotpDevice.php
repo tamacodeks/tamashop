148411c523e2091a930482424768a5e019e05e93
sydkhalid <sydkhalid7@gmail.com>
2024-06-06 00:07:36 +0530

    Update TotpDevice.php

--- app/Http/Middleware/TotpDevice.php
+++ app/Http/Middleware/TotpDevice.php
@@ -5,19 +5,23 @@ namespace App\Http\Middleware;
 use app\Library\AppHelper;
 use Closure;
 use Auth;
+use Session;
 
 class TotpDevice
 {
     public function handle($request, Closure $next)
     {
+        $user_id = \Session::get('impersonate');
+        if(!$user_id){
             if (in_array(auth()->user()->group_id, [3,4])) {
                 if(auth()->user()->enable_2fa == 0){
                    if(auth()->user()->verify_2fa != 1){
-                       return redirect()->to('enable-2fa')->with('message',trans('common.access_violation'))
+                       return redirect()->to('enable-2fa')->with('message',trans('common.access_totp'))
                            ->with('message_type','warning');
                    }
                 }
             }
+        }
         return $next($request);
     }
 
