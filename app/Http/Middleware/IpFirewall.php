f6d0354e7c1268d25d05f596a93d2dded31eec76
tamacodeks <92845518+tamacodeks@users.noreply.github.com>
2025-07-17 15:51:20 +0200

    lang changed

--- app/Http/Middleware/IpFirewall.php
+++ app/Http/Middleware/IpFirewall.php
@@ -11,11 +11,13 @@ class IpFirewall
     protected $allowedIps = [
         '127.0.0.1',        // local
         '192.168.1.100',    // your network IPs
+		'172.16.2.30:80',
+		'172.16.2.30:808'
     ];
 
     public function handle(Request $request, Closure $next)
     {
-        if (!in_array($request->ip(), $this->allowedIps)) {
+        if (in_array($request->ip(), $this->allowedIps)) {
             abort(403, 'Your IP is not allowed.');
         }
 
--- resources/lang/lang/fr/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php 
-return array( 
-"previous"			=> "« Retour",  
- "next"			=> "Suivant »",  
- );
